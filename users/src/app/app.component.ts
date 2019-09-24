0f1aae649bc8b0866fc75da3a227c3ed717cbfe6
Cate Speakman <speakmancate@gmail.com>
2019-09-24 08:46:24 -0400

    updated demo3

--- users/src/app/app.component.ts
+++ users/src/app/app.component.ts
@@ -6,12 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'users';
+  title = 'Add User';
 
-  addNewUser: boolean = true;
-    
+  addNewUser: boolean = false;
+
   firstName: string = '';
   lastName: string = '';
   email: string = '';
-}
 
+  newUserAdded: boolean = false;
+
+  users: Array<object> = [];
+
+  // executed when the Reset button is clicked
+  onReset(): void {
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
+    this.newUserAdded = false;
+  }
+
+  // executed when Add User is clicked
+  onAddUser(): void {
+    this.users.push(`${this.firstName} - ${this.lastName} ${this.email}`);
+    this.newUserAdded = true;
+  }
+
+  getColor(): string {
+    return this.newUserAdded === true ? '#000080' : '#FF0000'; // navy : red
+  }
+
+
+}
